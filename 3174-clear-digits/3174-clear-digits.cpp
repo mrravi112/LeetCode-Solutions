7889a2c1489aa3deb925b882b8accad59ecac7e1
Mr.Ravi <ravikumar232223@gmail.com>
2026-06-24 13:27:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.34%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string st = "";
+        for (char c : s) {
+            if (isdigit(c)) {
+                if (!st.empty()) st.pop_back();
+            } else {
+                st.push_back(c);
+            }
+        }
+        return st;
+    }
+};
+
